cb51cfd218c0735d1889494c7d475566f14eb302
rui.ishikawa <rui.ishikawa.business@gmail.com>
2020-09-27 22:54:20 +0900

    サンプルAPI追加

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,20 @@ import express from 'express';
 const app = express();
 app.get('/', (req, res) => { res.send('Hello World!!'); });
 
-// ユニットテスト時の呼び出しでなければサーバを起動する
-if(!module.parent) {
-  app.listen(process.env.PORT || 8080, () => {
-    console.log('Listening');
-  });
-}
+app.get('/api/v1/list', (req, res) => {
+  // クライアントに送るJSONデータ
+  const todoList = [
+      { title: 'JavaScriptを勉強する', done: true },
+      { title: 'Node.jsを勉強する', done: false },
+      { title: 'Web APIを作る', done: false }
+  ];
+  // JSONを送信する
+  res.json(todoList);
+});
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log('Listening on port 8080');
+});
 
 // ユニットテストのために app をエクスポートする
 export default app;
